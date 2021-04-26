cece2d08d9a917e193dd825750765a0f9147d9dc
sonia chander <soniachander7294@gmail.com>
2021-04-26 12:19:11 -0400

    My first project

---
+++ src/test/java/testingtestngwithmaven/Customerprofile.java
@@ -0,0 +1,11 @@
+package testingtestngwithmaven;
+
+import org.testng.annotations.Test;
+
+public class Customerprofile {
+
+	@Test
+	public void test1() {
+		System.out.println("my customer profile");
+	}
+}
